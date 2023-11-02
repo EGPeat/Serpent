8fdc9de81c1ef24e951daa517b2ad12d4a7785ae
EGPeat <87342021+EGPeat@users.noreply.github.com>
2023-11-02 17:10:24 +1030

    endian conversion added

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import serpentfunctions as sf
-import icecream as ic
+from icecream import ic
+
+# a,b = b, a
+"""print("Please enter 128, 192, 256 for your keylength: ")
+length = sf.input_val(2, int)
+print("Please enter a numerical seed for the RNG: ")
+rng = sf.input_val(16, int)
+key = sf.generate_key(length, rng)"""
+key = sf.generate_key()
+
+test = sf.binary_from_file()
+ic(len(test))
+ic(test)
+
+split_into_128 = sf.turn_into_128(test)
+ic(split_into_128)
--- serpentfunctions.py
+++ serpentfunctions.py
@@ -1,6 +1,26 @@
-import icecream as ic
+from icecream import ic
+from random import randint, seed
+import sys
 
-def inputVal(maxSize=16, valType=int, options=False):  # unused
+
+def p(info):
+    print(info)
+
+
+def pt(info):
+    print(type(info))
+
+
+def generate_key(key_len=128, sneed=2023):
+    seed(sneed)
+    key_bin = '0b'
+    for idx in range(key_len):
+        key_bin += str(randint(0, 1))
+
+    return int(key_bin, 2)
+
+
+def input_val(maxSize=16, valType=int, options=False):  # unused
 
     while True:
         try:
@@ -26,3 +46,36 @@ def inputVal(maxSize=16, valType=int, options=False):  # unused
             info = f"It seems you entered something that isn't a {valType}. Please try again"
         except IndexError:
             info = "you put in something that is a larger int than is allowed, or a longer string than is allowed"
+
+
+def binary_from_file(default_choice=True):
+    if default_choice is False:
+        print("Please enter the filename you wish to encrypt, excluding the .txt: ")
+        filename = input_val(32, str)
+    else:
+        filename = "sampletext"
+    with open(f"{filename}.txt", 'rb') as file:
+        info = file.read()
+        ic(type(info))
+    return info
+
+
+def turn_into_128(binary):
+    binary = bytearray(binary)
+    endian_value = sys.byteorder
+    full_blocks = len(binary) // 16
+    partial_blocks = len(binary) % 16
+    blocks128 = [None] * (full_blocks + 1)
+    binary += b'\x00' * (16 - partial_blocks)
+
+    if endian_value == "little":
+        for idx in range(full_blocks + 1):
+            blocks128[idx] = binary[(0 + 16 * idx):(16 + 16 * idx)]
+    else:  # need to add in endian swapping for bits as well as bytes. Not entirely sure this works
+        for idx in range(full_blocks + 1):
+            blocks128[idx] = bytearray(binary[(3)::-1] + binary[(7):(3):-1] + binary[(11):(7):-1] + binary[(15):(11):-1])
+            del binary[0:16]
+            for idx2 in range(16):
+                blocks128[idx][idx2] = int(('{:08b}'.format(blocks128[idx][idx2])[::-1]), 2)
+
+    return blocks128
