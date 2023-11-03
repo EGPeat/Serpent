07c193bc05a6ce8ed5dd2237ece720af5a55664f
EGPeat <87342021+EGPeat@users.noreply.github.com>
2023-11-03 21:51:27 +1030

    small changes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import serpentfunctions as sf
 from icecream import ic
 import bitstring as bts
-# a,b = b, a
 
-# this is not a bitslice form of anything
+
 """print("Please enter 128, 192, 256 for your keylength: ")
 length = sf.input_val(2, int)
 print("Please enter a numerical seed for the RNG: ")
@@ -23,27 +22,34 @@ for idx in range(len(full_keys_not_ba)):
     full_keys.append(bts.BitArray(uint=(int(full_keys_not_ba[idx], 2)), length=128))
 
 
-binary_data, filename = sf.binary_from_file()
-padded_info = sf.endian_and_padding(binary_data)
+binary_data, filename = sf.binary_from_file()  # change
+padded_info = bts.BitArray(sf.endian_and_padding(binary_data))
+print(len(padded_info))
+
+
+need_1000 = 0
+fp_tabled_data = bts.BitArray()
+word_ba = list()
+sboxed_128 = bts.BitArray(uint=0, length=128)
+
 
 with open(f"{filename}encoded.txt", "wb") as outputfile:
     while padded_info:
-        data_bitarray = bts.BitArray(padded_info[0:16])
-        del padded_info[0:16]
-        data_bitarray2 = bts.BitArray(data_bitarray)
+        data_bitarray = padded_info[0:128].copy()
+        del padded_info[0:128]
+        need_1000 += 16
         data_128_IPTabled = sf.IPTable_func(data_bitarray)
 
         for idx in range(32):
-            xored_ba = full_keys[idx] ^ data_128_IPTabled
-            word_ba = list()
-            sboxed_128 = bts.BitArray(uint=0, length=128)
+            data_128_IPTabled ^= full_keys[idx]      
             for split_idx in range(4):
-                word_ba = xored_ba[0 + (split_idx * 32):32 + (split_idx * 32)]
+                word_ba = data_128_IPTabled[0 + (split_idx * 32):32 + (split_idx * 32)]
                 sboxed_128[0 + (split_idx * 32):32 + (split_idx * 32)] = sf.sbox_function(word_ba, 0)
             data_128_IPTabled = sf.lin_transform(sboxed_128)
-        fp_tabled_data = sf.IPTable_func(data_128_IPTabled)
-        fp_tabled_data.tofile(outputfile)
+        data_128_IPTabled = data_128_IPTabled ^ full_keys[32]
+        fp_tabled_data.append(sf.FPTable_func(data_128_IPTabled))
+        if need_1000 >= 1000:
+            fp_tabled_data.tofile(outputfile)
+            fp_tabled_data.clear()
+            need_1000 = 0
 
-
-# data_128_FPTabled = sf.FPTable_func(data_128_IPTabled)
-# ic(data_128_FPTabled)
